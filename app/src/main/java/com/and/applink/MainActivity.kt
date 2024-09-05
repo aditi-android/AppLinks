5ffb7a57a64cb9f1c910efc715e8e76cb9445b1f
aditi-android <79632065+aditi-android@users.noreply.github.com>
2024-09-05 16:12:00 +0530

    Update MainActivity.kt

--- app/src/main/java/com/and/applink/MainActivity.kt
+++ app/src/main/java/com/and/applink/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        System.out.println("rewrite");
+        System.out.println("Main");
     }
 }
